0c3f864141378c8ecded1a9da0a76d726b6395ad
bagasdany <bagasdany74@gmail.com>
2020-11-01 12:08:32 +0700

    drawer tab

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font'
+import {AppLoading} from "expo"
+import {enableScreens} from "react-native-screens";
+import Navigation from "./navigations/SimasetNavigation";
 
+enableScreens()
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans":require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold":require("./assets/fonts/OpenSans-Bold.ttf")
+  })
+}
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontLoaded,setFontloaded] = useState(false)
+  if(!fontLoaded){
+    return (
+      <AppLoading
+      startAsync={fetchFonts}
+      onFinish={()=>setFontloaded(true)}
+      onError={(err)=>{
+        console.log(err)
+      }}
+      />
+    )
+    
+  }
+  return <Navigation/>
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +37,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  text:{
+    fontFamily:'open-sans-bold'
+  }
 });
